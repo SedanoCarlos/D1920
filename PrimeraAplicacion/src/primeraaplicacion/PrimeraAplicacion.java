f498f475ede2963c8ca908477cfda8845ed17eb2
DAM216 <DAM216@PORTB609.iesataulfo.local>
2019-10-18 15:38:06 +0200

    Hola mundo

--- PrimeraAplicacion/src/primeraaplicacion/PrimeraAplicacion.java
+++ PrimeraAplicacion/src/primeraaplicacion/PrimeraAplicacion.java
@@ -15,7 +15,7 @@ public class PrimeraAplicacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
